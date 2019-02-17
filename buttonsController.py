b854010e276321e37d70a48f1c94a4300a264261
Johnni1208 <developer@johnlouisjacobs.de>
2019-02-17 21:19:21 +0100

    Moved everything button related to own file

---
+++ buttonsController.py
@@ -0,0 +1,62 @@
+from pygame import time as time
+
+from buttons.youtubeButton import YoutubeButton
+from buttons.netflixButton import NetflixButton
+from buttons.spotifyButton import SpotifyMainButton
+from buttons.media.playAndPauseButton import PlayAndPauseButton
+from buttons.media.nextTrackButton import NextTrackButton
+from buttons.media.previousTrackButton import PreviousTrackButton
+
+
+def setup_buttons(lp):
+    # Init youtube button
+    ytButton = YoutubeButton(lp)
+
+    # Init Netflix button
+    netflixButton = NetflixButton(lp)
+
+    # Init Spotify button
+    spotifyButton = SpotifyMainButton(lp)
+
+    # Init Play / Pause Button
+    playPauseButton = PlayAndPauseButton(lp)
+
+    # Init Next Track Button
+    nextTrackButton = NextTrackButton(lp)
+
+    # Init Previous Track Button
+    prevTrackButton = PreviousTrackButton(lp)
+
+    while True:
+        clickedButton = lp.ButtonStateXY()
+        if clickedButton and clickedButton[2] == 127:
+            # x = buts[0]
+            # y = buts[1]
+            # lp.LedCtrlXYByCode(x, y, random.randint(4, 100))
+            # if buts == [8, 7, 127]:
+            #     lp.Reset()
+
+            if clickedButton == ytButton.get_position():
+                ytButton.clicked()
+
+            if clickedButton == netflixButton.get_position():
+                netflixButton.clicked()
+
+            if clickedButton == spotifyButton.get_position():
+                spotifyButton.clicked()
+
+            if clickedButton == playPauseButton.get_position():
+                playPauseButton.clicked()
+
+            if clickedButton == nextTrackButton.get_position():
+                nextTrackButton.clicked()
+
+            if clickedButton == prevTrackButton.get_position():
+                prevTrackButton.clicked()
+
+            # Cancel program
+            if clickedButton == [8, 8, 127]:
+                lp.LedAllOn(79)
+                time.wait(500)
+                lp.Reset()
+                break
--- main.py
+++ main.py
@@ -1,75 +1,25 @@
 import launchpad_py as launchpad
 from pygame import time as time
-
-from buttons.youtubeButton import YoutubeButton
-from buttons.netflixButton import NetflixButton
-from buttons.spotifyButton import SpotifyMainButton
-from buttons.media.playAndPauseButton import PlayAndPauseButton
-from buttons.media.nextTrackButton import NextTrackButton
-from buttons.media.previousTrackButton import PreviousTrackButton
-
+from buttonsController import setup_buttons
 
 lp = launchpad.LaunchpadMk2()
+
+# Tests if Launchpad is connected
 if lp.Open(0, "mk2"):
     print(" - Launchpad Mk2: Ok")
 else:
     print(" - Launchpad Mk2: Could not find Launchpad")
+
+# Flushes the Launchpad, in case of it having anything saved
 lp.ButtonFlush()
 
+# Shows a short orange flash so the user sees that the program is ready
 lp.LedAllOn(500)
 time.wait(500)
 lp.LedAllOn(0)
 
-# Init youtube button
-ytButton = YoutubeButton(lp)
+# Sets up all buttons
+setup_buttons(lp)
 
-# Init Netflix button
-netflixButton = NetflixButton(lp)
-
-# Init Spotify button
-spotifyButton = SpotifyMainButton(lp)
-
-# Init Play / Pause Button
-playPauseButton = PlayAndPauseButton(lp)
-
-# Init Next Track Button
-nextTrackButton = NextTrackButton(lp)
-
-# Init Previous Track Button
-prevTrackButton = PreviousTrackButton(lp)
-
-while True:
-    clickedButton = lp.ButtonStateXY()
-    if clickedButton and clickedButton[2] == 127:
-        # x = buts[0]
-        # y = buts[1]
-        # lp.LedCtrlXYByCode(x, y, random.randint(4, 100))
-        # if buts == [8, 7, 127]:
-        #     lp.Reset()
-
-        if clickedButton == ytButton.get_position():
-            ytButton.clicked()
-
-        if clickedButton == netflixButton.get_position():
-            netflixButton.clicked()
-
-        if clickedButton == spotifyButton.get_position():
-            spotifyButton.clicked()
-
-        if clickedButton == playPauseButton.get_position():
-            playPauseButton.clicked()
-
-        if clickedButton == nextTrackButton.get_position():
-            nextTrackButton.clicked()
-
-        if clickedButton == prevTrackButton.get_position():
-            prevTrackButton.clicked()
-
-        # Cancel program
-        if clickedButton == [8, 8, 127]:
-            lp.LedAllOn(79)
-            time.wait(500)
-            lp.Reset()
-            break
-
+# If the program gets terminated close the Launchpad connection
 lp.Close()
